30d9b61b6a436295b12abcea30285c37042ac1b4
Mynul Alam <mynulalam27@gmail.com>
2025-07-31 23:28:42 +0600

    True false none

--- variable.py
+++ variable.py
@@ -5,3 +5,10 @@ isStudent = True
 habeis =["Coding", "Traveling", "Football"]
 info ={"Name":name, "age":age,}
 print(name,age,isStudent,height,habeis,info)
+
+#true, false,none
+
+isCSE = True
+isGraduate = False
+MidleName = None
+print(isCSE,isGraduate,MidleName)
