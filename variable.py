c6e137254fab83557fca8845be9e78f00197eadf
Mynul Alam <mynulalam27@gmail.com>
2025-07-31 23:24:32 +0600

    variable

---
+++ variable.py
@@ -0,0 +1,7 @@
+name ="Mynul Alam"
+age = 24
+height = 5.4
+isStudent = True
+habeis =["Coding", "Traveling", "Football"]
+info ={"Name":name, "age":age,}
+print(name,age,isStudent,height,habeis,info)
